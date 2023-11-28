3f266d202e9d9bcfc4d64e0dd66f42f2c0edccce
Megan Lewis <nxk7tq@virginia.edu>
2023-11-28 13:26:56 -0500

    made both MyReviews buttons in CourseSearch work, and created some methods to check for valid search input

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -20,12 +20,17 @@ public class CourseSearchController {
     private TabPane tabPane;
 
     @FXML
-    private Button addCourseTabButton;
-    @FXML
-    private Button selectSearchTabButton;
+    private Button addCourseTabButton, selectSearchTabButton;
 
     @FXML
-    private Button myReviewsButton;
+    private Label Username;
+
+    @FXML
+    private TextField searchSubjectTextField, searchNumberTextField, searchTitleTextField;
+
+
+    @FXML
+    Label errorLabel;
 
     public void initialize() {
         addCourseTabButton.setOnAction(e -> selectTab("Add Course"));
@@ -39,6 +44,15 @@ public class CourseSearchController {
                 .ifPresent(tab -> tabPane.getSelectionModel().select(tab));
     }
 
+    public void myReviewsButtonClick(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("MyReviews.fxml"));
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setTitle("Log in");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void logOutAccountClick(ActionEvent e) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
@@ -48,12 +62,70 @@ public class CourseSearchController {
         stage.show();
     }
 
-    public void myReviewsButtonClick(ActionEvent e) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("MyReviews.fxml"));
-        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setTitle("Log in");
-        stage.setScene(scene);
-        stage.show();
+    public void handleAddCourseButtonClick(){
+
+    }
+
+    public void handleSearchButtonClick(){
+        if (validCourseInput()){
+            System.out.println("Valid");
+            //code to search for and display courses that fit search criteria
+            //for title look for matching substrings (i.e., don't require exact titles
+            //for subject and number, require exact letter matches
+
+        }
+
+    }
+
+    private boolean validCourseInput(){
+        String subject = searchSubjectTextField.getText();
+        String number = searchNumberTextField.getText();
+        String title = searchTitleTextField.getText();
+
+        //subject-- 2-4 letters, store in database as uppercase
+        if(!validSubject(subject)){
+            System.out.println("Bad subject");
+            return false;
+
+        }
+        //
+        if(!validNumber(number)){
+            System.out.println("Bad number");
+            return false;
+        }
+    /*    if(title){
+            System.out.println("Bad number");
+            return false;
+        }*/
+        //number 4 digits
+        return true;
+    }
+
+    private boolean validSubject(String subject){
+        //errorLabel.setText("");
+        if(subject.isEmpty()){
+            return true;
+        }
+        if(!subject.matches("[a-zA-Z]+")){
+            return false;
+        }
+        if(subject.length() < 2 || subject.length() > 4){
+            return false;
+        }
+        //
+        return true;
+    }
+
+    private boolean validNumber(String number){
+        if(number.isEmpty()){
+            return true;
+        }
+        if(!number.matches("\\d+")){
+            return false;
+        }
+        if(number.length() < 2 || number.length() > 4){
+            return false;
+        }
+        return true;
     }
 }
