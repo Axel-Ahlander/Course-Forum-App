88560a7128203608fe623ecfb94737c17b72aaa5
Megan Lewis <nxk7tq@virginia.edu>
2023-11-28 17:18:23 -0500

    added working error messages to add course tab and select search tab, can probably be simplified later

--- src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
+++ src/main/java/edu/virginia/sde/reviews/CourseSearchController.java
@@ -23,23 +23,23 @@ public class CourseSearchController {
     private Button addCourseTabButton, selectSearchTabButton;
 
     @FXML
-    private Label Username;
+    private Label username, searchErrorLabel, addCourseErrorLabel;
 
     @FXML
-    private TextField searchSubjectTextField, searchNumberTextField, searchTitleTextField;
+    private TextField searchSubjectTextField, searchNumberTextField, searchTitleTextField, addCourseSubjectTextField, addCourseNumberTextField, addCourseTitleTextField;
 
-
-    @FXML
-    Label errorLabel;
 
     public void initialize() {
-        addCourseTabButton.setOnAction(e -> selectTab("Add Course"));
+        addCourseTabButton.setOnAction(e -> selectTab());
         selectSearchTabButton.setOnAction(e -> tabPane.getSelectionModel().select(0));
+
+        searchErrorLabel.setText("");
+        addCourseErrorLabel.setText("");
     }
 
-    private void selectTab(String tabText) {
+    private void selectTab() {
         tabPane.getTabs().stream()
-                .filter(tab -> tab.getText().equals(tabText))
+                .filter(tab -> tab.getText().equals("Add Course"))
                 .findFirst()
                 .ifPresent(tab -> tabPane.getSelectionModel().select(tab));
     }
@@ -62,12 +62,94 @@ public class CourseSearchController {
         stage.show();
     }
 
+
     public void handleAddCourseButtonClick(){
+        addCourseErrorLabel.setText("");
+        //subject-- 2-4 characters
+
+        //number-- 4 digits exactly
+        //title-- 1-50 characters
+
+        //check if course already is in table
+        if (validAddCourseInput()){
+            System.out.println("Valid");
+            //code to search for and display courses that fit search criteria
+            //for title look for matching substrings (i.e., don't require exact titles
+            //for subject and number, require exact letter matches
+
+        /*    if(doesCourseExistAlready){ //check if course exists, and add it
+                //add course
+            }
+            else{
+                //error message about the course already being added
+            }
+         */
+
+
+        }
+    }
+
+    private boolean validAddCourseInput(){
+        String subject = addCourseSubjectTextField.getText();
+        String number = addCourseNumberTextField.getText();
+        String title = addCourseTitleTextField.getText();
+        //subject-- 2-4 characters
+        if(!validAddCourseSubject(subject)){
+            return false;
+        }
+        if(!validAddCourseNumber(number)){
+            return false;
+        }
+        if(!validAddCourseTitle(title)){
+            return false;
+        }
+        //number-- 4 digits exactly
+        //title-- 1-50 characters
+        return true;
+    }
+    private boolean validAddCourseSubject(String subject){
+        if(subject.isEmpty()){
+            addCourseErrorLabel.setText("Must enter a course subject");
+            return false;
+        }
+        if(!subject.matches("[a-zA-Z]+")){
+            addCourseErrorLabel.setText("Subject must be letters only");
+            return false;
+        }
+        if(subject.length() < 2 || subject.length() > 4){
+            addCourseErrorLabel.setText("Subject must be 2-4 letters long");
+            return false;
+        }
+        return true;
 
     }
 
+    private boolean validAddCourseNumber(String number){
+        if(number.isEmpty()){
+            addCourseErrorLabel.setText("Must enter a course number");
+            return false;
+        }
+        if(!number.matches("\\d+")){
+            addCourseErrorLabel.setText("Course number must be numbers only");
+            return false;
+        }
+        if(number.length() != 4){
+            addCourseErrorLabel.setText("Course number must be exactly 4 digits");
+            return false;
+        }
+        return true;
+    }
+    private boolean validAddCourseTitle(String title){
+        if(title.isEmpty() || title.length() > 50){
+            addCourseErrorLabel.setText("Course title must be 1-50 characters long");
+            return false;
+        }
+        return true;
+    }
+
     public void handleSearchButtonClick(){
-        if (validCourseInput()){
+        searchErrorLabel.setText("");
+        if (validSearchInput()){
             System.out.println("Valid");
             //code to search for and display courses that fit search criteria
             //for title look for matching substrings (i.e., don't require exact titles
@@ -76,56 +158,62 @@ public class CourseSearchController {
         }
 
     }
-
-    private boolean validCourseInput(){
+    private boolean validSearchInput(){
         String subject = searchSubjectTextField.getText();
         String number = searchNumberTextField.getText();
         String title = searchTitleTextField.getText();
 
-        //subject-- 2-4 letters, store in database as uppercase
-        if(!validSubject(subject)){
+        if(!validSearchSubject(subject)){
             System.out.println("Bad subject");
             return false;
 
         }
-        //
-        if(!validNumber(number)){
+        if(!validSearchNumber(number)){
             System.out.println("Bad number");
             return false;
         }
-    /*    if(title){
-            System.out.println("Bad number");
-            return false;
-        }*/
-        //number 4 digits
-        return true;
+        return validSearchTitle(title);
     }
 
-    private boolean validSubject(String subject){
-        //errorLabel.setText("");
+    private boolean validSearchTitle(String title){
+        if(title.isEmpty()){
+            return true;
+        }
+        if(title.length() > 50){
+            searchErrorLabel.setText("Title must be 50 characters or less");
+            return false;
+        }
+        return true;
+    }
+    private boolean validSearchSubject(String subject){
         if(subject.isEmpty()){
             return true;
         }
         if(!subject.matches("[a-zA-Z]+")){
+            searchErrorLabel.setText("Subject must be letters only");
             return false;
         }
-        if(subject.length() < 2 || subject.length() > 4){
+        if(subject.length() > 4){
+            searchErrorLabel.setText("Course subject cannot exceed 4 letters");
             return false;
         }
-        //
         return true;
     }
 
-    private boolean validNumber(String number){
+    private boolean validSearchNumber(String number){
         if(number.isEmpty()){
             return true;
         }
         if(!number.matches("\\d+")){
+            searchErrorLabel.setText("Course number must be numbers only");
             return false;
         }
-        if(number.length() < 2 || number.length() > 4){
+        if(number.length() > 4){
+            searchErrorLabel.setText("Course number cannot exceed 4 digits");
             return false;
         }
         return true;
     }
+
 }
+
